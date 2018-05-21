2a88ae256a5a896a9d0dd29c61284afa84da7a45
Maxime <maxim.beaudoin@gmail.com>
2018-05-21 10:44:27 -0400

    included redux saga

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -9,8 +9,13 @@ import { InputWithButton } from "../components/TextInput";
 import { ClearButton } from "../components/Buttons";
 import { LastConverted } from "../components/Text";
 import { Header } from "../components/Header";
+import { connectAlert } from "../components/Alert";
 
-import { swapCurrency, changeCurrencyAmount } from "../actions/currencies";
+import {
+  swapCurrency,
+  changeCurrencyAmount,
+  getInitialConversion
+} from "../actions/currencies";
 
 class Home extends Component {
   static propTypes = {
@@ -22,8 +27,23 @@ class Home extends Component {
     conversionRate: PropTypes.number,
     isFetching: PropTypes.bool,
     lastConvertedDate: PropTypes.object,
-    primaryColor: PropTypes.string
+    primaryColor: PropTypes.string,
+    alertWithType: PropTypes.func,
+    currencyError: PropTypes.string
   };
+
+  componentWillMount() {
+    this.props.dispatch(getInitialConversion());
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (
+      nextProps.currencyError &&
+      nextProps.currencyError !== this.props.currencyError
+    ) {
+      this.props.alertWithType("error", "Error", nextProps.currencyError);
+    }
+  }
 
   handlePressBaseCurrency = () => {
     this.props.navigation.navigate("CurrencyList", {
@@ -111,8 +131,9 @@ const mapStateToProps = state => {
     lastConvertedDate: conversionSelector.date
       ? new Date(conversionSelector.date)
       : new Date(),
-    primaryColor: state.theme.primaryColor
+    primaryColor: state.theme.primaryColor,
+    currencyError: state.currencies.error
   };
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps)(connectAlert(Home));
